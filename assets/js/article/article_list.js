ecbf67df0310d01505a347797927cb1b7ab56dc2
zht <1683412300@qq.com>
2022-04-26 22:59:32 +0800

    完成文章管理功能的开发

---
+++ assets/js/article/article_list.js
@@ -0,0 +1,126 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+    initTable()
+    initCate()
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取列表数据失败!')
+                }
+                var htmlStr = template('tpl_table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
+            }
+        })
+    }
+
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('')
+                }
+                var htmlStr = template('tpl_cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+    $("#form-search").on('submit', function(e) {
+            e.preventDefault()
+            var cate_id = $('[name=cate_id]').val()
+            var state = $('[name=state]').val()
+            q.cate_id = cate_id
+            q.state = state
+            initTable()
+        })
+        // 定义渲染分页效果
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox', // 分页容器的 Id
+            count: total, // 总数据条数
+            limit: q.pagesize, // 每页显示几条数据
+            curr: q.pagenum, // 设置默认被选中的分页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 8, 10],
+            jump: function(obj, first) {
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+    $('tbody').on('click', '.btn-delete', function() {
+        // 获取删除按钮的个数
+        var len = $('.btn-delete').length
+        console.log(len)
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                        // 当数据删除完成后，需要判断当前这一中，是否还有剩余的数据
+                        // 如果没有剩余的数据了,则让页码值 -1 后,
+                        // 再重新调用 initTable 方法
+                    if (len === 1) {
+                        // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
+                        // 页码值最小必须是 1
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+
+            layer.close(index)
+        })
+    })
+    $('tbody').on('click', '.btn-edit', function() {
+        var id = $(this).attr('data-id')
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/' + id,
+            success: function(res) {
+                location.href = '/article/article_pub.html'
+            }
+        })
+    })
+})
